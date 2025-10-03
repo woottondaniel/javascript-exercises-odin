4fe14686431d13f8a567b9311f0cc28f9ce30b3b
Daniel Wootton <225040828+woottondaniel@users.noreply.github.com>
2025-10-03 12:38:28 +0200

    exercise 1 done

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -2,6 +2,6 @@ const helloWorld = require('./helloWorld');
 
 describe('Hello World', function() {
   test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
+    expect(helloWorld('Hello, World!')).toEqual('Hello, World!');
   });
 });
